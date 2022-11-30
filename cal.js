b7d32fff22978b8d611303c932343566e1e0bc54
sharonlalg1234 <sharon3lok@gmail.com>
2022-11-30 16:39:55 +0530

    calculator v1

---
+++ cal.js
@@ -0,0 +1,13 @@
+function display(num){
+console.log(result);
+result.value += num
+}
+function allClr(){
+    result.value=""
+}
+function equal(){
+    result.value(result.value)
+}
+function back(){
+    result.value=result.value.slice(0,-1)
+}
